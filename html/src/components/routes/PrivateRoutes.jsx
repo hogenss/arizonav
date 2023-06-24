62baead6bab47bdd6ce998f026f4c2d9c078613e
Pin-ky <lololoilo1@yandex.ru>
2023-06-24 19:55:38 +0700

    401

--- html/src/components/routes/PrivateRoutes.jsx
+++ html/src/components/routes/PrivateRoutes.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import {Outlet} from "react-router-dom";
 import cl from './PrivateRoutes.module.css'
 import NavBar from "../NavBar";
-// import SlideBar from "../SlideBar/SlideBar";
-// import MiniProfile from "../MiniProfile/MiniProfile";
 
 const PrivateRoutes = () => {
 
--- html/src/http/index.js
+++ html/src/http/index.js
@@ -8,4 +8,20 @@ const $api = axios.create({
     baseURL: API_URL
 })
 
+$api.interceptors.response.use((config) => {
+    return config;
+},async (error) => {
+    const originalRequest = error.config;
+    if (error.response.status == 401 && error.config && !error.config._isRetry) {
+        originalRequest._isRetry = true;
+        try {
+            window.location.href = `${API_URL}/discord`
+            return $api.request(originalRequest);
+        } catch (e) {
+            console.log('unauthorized')
+        }
+    }
+    throw error;
+})
+
 export default  $api;
